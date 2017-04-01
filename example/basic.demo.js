fb3bad804dff7033ba100cedde6a73fd73a17baf
Nimo <nimo.jser@gmail.com>
2017-04-01 18:05:39 +0800

    test

--- example/basic.demo.js
+++ example/basic.demo.js
@@ -1,9 +1,67 @@
 var React = require('react')
-var Pickermode = require('picker-mode').default
+var Picker = require('picker-mode').default
+// import Picker from "picker-mode"
+
+var Item = React.createClass({
+    render: function () {
+        var self = this
+        return (
+            <label style={{cursor: 'pointer', display: 'block'}} >
+                <input type="checkbox" checked={self.props.checked} onChange={function (e) {
+                        self.props.onChange(self.props.id, e.target.checked)
+                    }} />
+                {
+                    self.props.text
+                }
+            </label>
+        )
+    }
+})
 var App = React.createClass({
     getInitialState: function () {
         return {
-            show: true
+            data:[]
+        }
+    },
+    picker: null,
+    componentWillMount () {
+        var self = this
+        var state = self.state
+        var data = [
+            {
+                id: 'f23iuh23bff2',
+                text:'mail@qq.com'
+            },
+            {
+                id: '3tufh23ifghuw',
+                text: 'abc@163.com'
+            },
+            {
+                id: 'g24hg823hg232',
+                text: '999@gmail.com'
+            },
+            {
+                id: 'g32igh2iughh3',
+                text: 'wehfiu2q3@tom.cc'
+            }
+        ]
+        state.data = data
+        self.picker = new Picker({
+            data: data,
+            onChange: function (stat) {
+                console.log(stat)
+            }
+        })
+
+        self.setState(state)
+    },
+    change: function (id, checked) {
+        const self = this
+        if (checked) {
+            self.picker.checked(id)
+        }
+        else {
+            self.picker.uncheck(id)
         }
     },
     render: function () {
@@ -11,14 +69,11 @@ var App = React.createClass({
         return (
             <div>
                 {
-                    self.state.show?
-                    (
-                        <Pickermode onClose={function() {
-                                self.setState({
-                                    show: false
-                                })
-                            }} >basic</Pickermode>
-                    ):null
+                    self.state.data.map(function(item, key) {
+                        return (
+                            <Item key={key} id={item.id} text={item.text} checked={item.checked} onChange={self.change} />
+                        )
+                    })
                 }
             </div>
         )
--- test/basic.test.js
+++ test/basic.test.js
@@ -1,14 +1,223 @@
-import { Component } from "react"
-import Pickermode from "../lib/index"
-import Test from 'react-addons-test-utils'
-import { delay } from "./util"
-it('className', function (done) {
-    var app = Test.renderIntoDocument(<Pickermode className="myclass" />)
-    delay(done,[
-        function checkClassName() {
-            expect(Test.scryRenderedDOMComponentsWithClass(app, 'myclass').length)
-                .toEqual(1)
-            arguments[0]()
-        }
-    ])
+import Picker from "../lib/index"
+
+// localStorage 缓存选中项 (还需要缓存当前是全局选择模式)
+
+describe('basic.test.js', function () {
+    it('checked', function () {
+        let data
+        var picker = new Picker({
+            data: [
+                {
+                    id: 'f23iuh23bff2',
+                    checked: false
+                },
+                {
+                    id: '3tufh23ifghuw',
+                    checked: false
+                },
+                {
+                    id: 'g24hg823hg232',
+                    checked: false
+                },
+                {
+                    id: 'g32igh2iughh3',
+                    checked: false
+                }
+            ],
+            onChange: function (stat) {
+                data = stat
+            }
+        })
+        expect(data).toEqual(
+            {
+                checked: [
+
+                ],
+                uncheck: [
+                    'f23iuh23bff2',
+                    '3tufh23ifghuw',
+                    'g24hg823hg232',
+                    'g32igh2iughh3'
+                ],
+                type: 'empty'
+            }
+        )
+        picker.checked('3tufh23ifghuw')
+        expect(data).toEqual(
+            {
+                checked: [
+                    '3tufh23ifghuw'
+                ],
+                uncheck: [
+                    'f23iuh23bff2',
+                    'g24hg823hg232',
+                    'g32igh2iughh3'
+                ],
+                type: 'some'
+            }
+        )
+        picker.checked('f23iuh23bff2')
+        expect(data).toEqual(
+            {
+                checked: [
+                    'f23iuh23bff2',
+                    '3tufh23ifghuw',
+
+                ],
+                uncheck: [
+                    'g24hg823hg232',
+                    'g32igh2iughh3'
+                ],
+                type: 'some'
+            }
+        )
+        picker.checked('g24hg823hg232')
+        expect(data).toEqual(
+            {
+                checked: [
+                    'f23iuh23bff2',
+                    '3tufh23ifghuw',
+                    'g24hg823hg232'
+
+                ],
+                uncheck: [
+                    'g32igh2iughh3'
+                ],
+                type: 'some'
+            }
+        )
+        picker.checked('g32igh2iughh3')
+        expect(data).toEqual(
+            {
+                checked: [
+                    'f23iuh23bff2',
+                    '3tufh23ifghuw',
+                    'g24hg823hg232',
+                    'g32igh2iughh3'
+                ],
+                uncheck: [],
+                type: 'all'
+            }
+        )
+        // 重复选择
+        picker.checked('g32igh2iughh3')
+        expect(data).toEqual(
+            {
+                checked: [
+                    'f23iuh23bff2',
+                    '3tufh23ifghuw',
+                    'g24hg823hg232',
+                    'g32igh2iughh3'
+                ],
+                uncheck: [],
+                type: 'all'
+            }
+        )
+    })
+    it('uncheck', function () {
+        var data
+        var picker = new Picker({
+            data: [
+                {
+                    id: 'f23iuh23bff2',
+                    checked: true
+                },
+                {
+                    id: '3tufh23ifghuw',
+                    checked: false
+                },
+                {
+                    id: 'g24hg823hg232',
+                    checked: true
+                },
+                {
+                    id: 'g32igh2iughh3',
+                    checked: false
+                }
+            ],
+            onChange: function (stat) {
+                data = stat
+            }
+        })
+        expect(data).toEqual(
+            {
+                checked: [
+                    'f23iuh23bff2',
+                    'g24hg823hg232'
+                ],
+                uncheck: [
+                    '3tufh23ifghuw',
+                    'g32igh2iughh3'
+                ],
+                type: 'some'
+            }
+        )
+        picker.uncheck('f23iuh23bff2')
+        expect(data).toEqual(
+            {
+                checked: [
+                    "g24hg823hg232"
+                ],
+                uncheck: [
+                    "f23iuh23bff2",
+                    "3tufh23ifghuw",
+                    "g32igh2iughh3"
+                ],
+                type:"some"
+            }
+        )
+        picker.uncheck('g24hg823hg232')
+        expect(data).toEqual(
+            {
+                checked: [],
+                uncheck: ['f23iuh23bff2', '3tufh23ifghuw', 'g24hg823hg232', 'g32igh2iughh3'],
+                type: 'empty'
+            }
+        )
+        picker.checked('f23iuh23bff2')
+        expect(data).toEqual(
+            {
+                checked: ['f23iuh23bff2'],
+                uncheck: ['3tufh23ifghuw', 'g24hg823hg232', 'g32igh2iughh3'],
+                type: 'some'
+            }
+        )
+    })
+    it('clear', function () {
+        var data
+        var picker = new Picker({
+            data: [
+                {
+                    id: 'f23iuh23bff2',
+                    checked: true
+                },
+                {
+                    id: '3tufh23ifghuw',
+                    checked: false
+                },
+                {
+                    id: 'g24hg823hg232',
+                    checked: true
+                },
+                {
+                    id: 'g32igh2iughh3',
+                    checked: false
+                }
+            ],
+            onChange: function (stat) {
+                data = stat
+            }
+        })
+        picker.clear()
+        expect(data).toEqual(
+            {
+                checked: [],
+                uncheck: [],
+                type: 'empty'
+            }
+        )
+    })
+    it('uncheckAll', function () {
+
+    })
 })
