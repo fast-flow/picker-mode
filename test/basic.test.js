c4672da9bc8a4167197a707258bd10f5759185b5
Nimo <nimo.jser@gmail.com>
2017-04-05 15:11:40 +0800

    修改为 mode="check uncheck" type ="empty some all"

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,7 +1,9 @@
 import Picker from "../lib/index"
 
 // localStorage 缓存选中项 (还需要缓存当前是全局选择模式)
-
+const commonTestGetViewData = function () {
+    return ['f23iuh23bff2', '3tufh23ifghuw', 'g24hg823hg232', 'g32igh2iughh3']
+}
 describe('basic.test.js', function () {
     it('checked', function () {
         let data
@@ -35,6 +37,7 @@ describe('basic.test.js', function () {
 
         expect(data).toEqual(
             {
+                mode : 'check',
                 checked: [
 
                 ],
@@ -47,12 +50,13 @@ describe('basic.test.js', function () {
                 type: 'empty'
             }
         )
-        // NOTE: 可通过 checked 选中某项，调用后 onChange会立即回调
-        picker.checked('3tufh23ifghuw')
+        // NOTE: 可通过 check 选中某项，调用后 onChange会立即回调
+        picker.check('3tufh23ifghuw')
         expect(data).toEqual(
             {
+                mode : 'check',
                 checked: [
-                    '3tufh23ifghuw'
+                    '3tufh23ifghuw',
                 ],
                 uncheck: [
                     'f23iuh23bff2',
@@ -62,53 +66,28 @@ describe('basic.test.js', function () {
                 type: 'some'
             }
         )
-        picker.checked('f23iuh23bff2')
-        expect(data).toEqual(
-            {
-                checked: [
-                    'f23iuh23bff2',
-                    '3tufh23ifghuw',
 
+        // NOTE: check 只会选中某些项，如果 stat.checked 中存在这一项，则数据不会做任何改变，但 onChange会被回调
+        picker.check('3tufh23ifghuw')
+        expect(data).toEqual(
+            {
+                mode : 'check',
+                checked: [
+                    '3tufh23ifghuw',
                 ],
                 uncheck: [
+                    'f23iuh23bff2',
                     'g24hg823hg232',
                     'g32igh2iughh3'
                 ],
                 type: 'some'
             }
         )
-        picker.checked('g24hg823hg232')
+        // NOTE: check() 可以传入数组
+        picker.check(['f23iuh23bff2', 'g24hg823hg232', 'g32igh2iughh3'])
         expect(data).toEqual(
             {
-                checked: [
-                    'f23iuh23bff2',
-                    '3tufh23ifghuw',
-                    'g24hg823hg232'
-
-                ],
-                uncheck: [
-                    'g32igh2iughh3'
-                ],
-                type: 'some'
-            }
-        )
-        picker.checked('g32igh2iughh3')
-        expect(data).toEqual(
-            {
-                checked: [
-                    'f23iuh23bff2',
-                    '3tufh23ifghuw',
-                    'g24hg823hg232',
-                    'g32igh2iughh3'
-                ],
-                uncheck: [],
-                type: 'all'
-            }
-        )
-        // NOTE: checked 只会选中某些项，如果 stat.checked 中存在这一项，则数据不会做任何改变，但 onChange会被回调
-        picker.checked('g32igh2iughh3')
-        expect(data).toEqual(
-            {
+                mode : 'check',
                 checked: [
                     'f23iuh23bff2',
                     '3tufh23ifghuw',
@@ -147,6 +126,7 @@ describe('basic.test.js', function () {
         })
         expect(data).toEqual(
             {
+                mode: 'check',
                 checked: [
                     'f23iuh23bff2',
                     'g24hg823hg232'
@@ -162,6 +142,7 @@ describe('basic.test.js', function () {
         picker.uncheck('f23iuh23bff2')
         expect(data).toEqual(
             {
+                mode: 'check',
                 checked: [
                     "g24hg823hg232"
                 ],
@@ -176,17 +157,29 @@ describe('basic.test.js', function () {
         picker.uncheck('g24hg823hg232')
         expect(data).toEqual(
             {
+                mode: 'check',
                 checked: [],
                 uncheck: ['f23iuh23bff2', '3tufh23ifghuw', 'g24hg823hg232', 'g32igh2iughh3'],
                 type: 'empty'
             }
         )
-        picker.checked('f23iuh23bff2')
+        picker.check(['f23iuh23bff2', '3tufh23ifghuw'])
         expect(data).toEqual(
             {
-                checked: ['f23iuh23bff2'],
-                uncheck: ['3tufh23ifghuw', 'g24hg823hg232', 'g32igh2iughh3'],
+                mode: 'check',
+                checked: ['f23iuh23bff2', '3tufh23ifghuw'],
+                uncheck: [ 'g24hg823hg232', 'g32igh2iughh3'],
                 type: 'some'
+            }
+        )
+        // NOTE: uncheck(id) 也支持数组
+        picker.uncheck(['f23iuh23bff2', '3tufh23ifghuw'])
+        expect(data).toEqual(
+            {
+                mode: 'check',
+                checked: [],
+                uncheck: ['f23iuh23bff2', '3tufh23ifghuw', 'g24hg823hg232', 'g32igh2iughh3'],
+                type: 'empty'
             }
         )
     })
@@ -218,59 +211,27 @@ describe('basic.test.js', function () {
         // NOTE: 用于删除某一项目， 一般是在用户点击删除某条数据时调用，删除所有数据请使用 picker.clear()
         picker.del('g24hg823hg232')
         expect(data).toEqual({
+            mode: 'check',
             checked: [],
             uncheck: ['f23iuh23bff2','3tufh23ifghuw', 'g32igh2iughh3'],
             type: 'empty'
         })
-        picker.checked('3tufh23ifghuw')
+        picker.check('3tufh23ifghuw')
         expect(data).toEqual({
+            mode: 'check',
             checked: ['3tufh23ifghuw'],
             uncheck: ['f23iuh23bff2', 'g32igh2iughh3'],
             type: 'some'
         })
         picker.del('3tufh23ifghuw')
         expect(data).toEqual({
+            mode: 'check',
             checked: [],
             uncheck: ['f23iuh23bff2', 'g32igh2iughh3'],
             type: 'empty'
         })
     })
-    it('clear', function () {
-        var data
-        var picker = new Picker({
-            data: [
-                {
-                    id: 'f23iuh23bff2',
-                    checked: true
-                },
-                {
-                    id: '3tufh23ifghuw',
-                    checked: false
-                },
-                {
-                    id: 'g24hg823hg232',
-                    checked: true
-                },
-                {
-                    id: 'g32igh2iughh3',
-                    checked: false
-                }
-            ],
-            onChange: function (stat) {
-                data = stat
-            }
-        })
-        // NOTE: clear 用于清除所有数据，一般在删除列表所有内容的时候才需要调用
-        picker.clear()
-        expect(data).toEqual(
-            {
-                checked: [],
-                uncheck: [],
-                type: 'empty'
-            }
-        )
-    })
-    it('checkedAll', function () {
+    it('checkAll', function () {
         var data
         var picker = new Picker({
             data: [
@@ -291,94 +252,25 @@ describe('basic.test.js', function () {
                     checked: false
                 }
             ],
-            // NOTE: getViewData 是当前页面中显示数据的 id （用于与 picker.checkedAll state.type="pickerAll" 关联）
-            getViewData: function () {
-                return [
-                    '3tufh23ifghuw',
-                    'g24hg823hg232',
-                    'g32igh2iughh3'
-                ]
-            },
+            // NOTE: getViewData 需要返回当前页面中显示数据的 id
+            getViewData: commonTestGetViewData,
             onChange: function (stat) {
                 data = stat
             }
         })
         expect(data).toEqual({
+            mode: 'check',
             checked: [],
             uncheck: ['f23iuh23bff2','3tufh23ifghuw', 'g24hg823hg232', 'g32igh2iughh3'],
             type: 'empty'
         })
-        // NOTE: checkedAll 用于选中 getViewData 中的数据，选中所有数据请使用 picker.checkedGlobalAll()
-        picker.checkedAll()
+        // NOTE: checkAll 用于选中所有数据，会使得 picker 进入反选模式
+        picker.checkAll()
         expect(data).toEqual({
-            checked: ['3tufh23ifghuw', 'g24hg823hg232', 'g32igh2iughh3'],
-            uncheck: ['f23iuh23bff2'],
-            type: 'some'
-        })
-    })
-    it('checkedGlobalAll', function () {
-        var data
-        var picker = new Picker({
-            data: [
-                {
-                    id: 'f23iuh23bff2',
-                    checked: false
-                },
-                {
-                    id: '3tufh23ifghuw',
-                    checked: false
-                },
-                {
-                    id: 'g24hg823hg232',
-                    checked: false
-                },
-                {
-                    id: 'g32igh2iughh3',
-                    checked: false
-                }
-            ],
-            getViewData: function () {
-                return [
-                    '3tufh23ifghuw',
-                    'g24hg823hg232',
-                    'g32igh2iughh3'
-                ]
-            },
-            onChange: function (stat) {
-                data = stat
-            }
-        })
-        expect(data).toEqual({
-            checked: [],
-            uncheck: ['f23iuh23bff2','3tufh23ifghuw', 'g24hg823hg232', 'g32igh2iughh3'],
-            type: 'empty'
-        })
-        picker.checkedGlobalAll()
-        expect(data).toEqual({
+            mode: 'uncheck',
             checked: ['f23iuh23bff2','3tufh23ifghuw', 'g24hg823hg232', 'g32igh2iughh3'],
             uncheck: [],
-            type: 'globalAll'
-        })
-        picker.uncheck('3tufh23ifghuw')
-        expect(data).toEqual({
-            checked: ['f23iuh23bff2', 'g24hg823hg232', 'g32igh2iughh3'],
-            uncheck: ['3tufh23ifghuw'],
-            type: 'globalSome'
-        })
-        picker.uncheck('f23iuh23bff2')
-        picker.uncheck('g24hg823hg232')
-        picker.uncheck('g32igh2iughh3')
-        expect(data).toEqual({
-            checked: [],
-            uncheck: ['f23iuh23bff2','3tufh23ifghuw', 'g24hg823hg232', 'g32igh2iughh3'],
-            type: 'empty'
-        })
-        picker.checked('f23iuh23bff2')
-        // 此处逻辑需要确认
-        expect(data).toEqual({
-            checked: ['f23iuh23bff2'],
-            uncheck: ['3tufh23ifghuw', 'g24hg823hg232', 'g32igh2iughh3'],
-            type: 'globalSome'
+            type: 'all'
         })
     })
 })
